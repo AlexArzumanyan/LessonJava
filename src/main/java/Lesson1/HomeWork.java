3b226f52fa061bf77b2cb27370cbb1308f90475b
Alexandr <alex.arzumanyan20@gmail.com>
2021-08-09 17:27:01 +0400

    Lesson1 done

--- src/main/java/Lesson1/HomeWork.java
+++ src/main/java/Lesson1/HomeWork.java
@@ -2,6 +2,54 @@ package Lesson1;
 
 public class HomeWork {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+
+
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+
+    }
+
+    public static void checkSumSign() {
+        int a = 25;
+        int b = 47;
+        int sum = a + b;
+
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        } else if (sum < 0) {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+
+    public static void printColor() {
+        int value = 25;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        } else if (value > 100) {
+            System.out.println("Зеленый");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = 24;
+        int b = 7;
+        if (a >=b) {
+            System.out.println("a>=b");
+        }
+        else if (a <= b) {
+            System.out.println("a<=b");
+        }
+
     }
 }
