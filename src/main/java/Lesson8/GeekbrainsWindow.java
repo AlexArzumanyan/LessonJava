35c8950550224c1fac610279db96d38b141003f3
Alexandr <alex.arzumanyan20@gmail.com>
2021-12-01 19:02:47 +0400

    Lesson8 done

--- src/main/java/Lesson8/GeekbrainsWindow.java
+++ src/main/java/Lesson8/GeekbrainsWindow.java
@@ -6,52 +6,55 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class GeekbrainsWindow extends JFrame {
-   private int randomNubmer;
-
+    private int randomNubmer;
+    private JTextField textField;
 
 
     public GeekbrainsWindow() {
 
 
-        this.randomNubmer = (int) (Math.random() * 10) +1;
+        this.randomNubmer = (int) (Math.random() * 10) + 1;
         setTitle("Игра: Угадай число");
         setBounds(600, 300, 600, 160);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setResizable(false);
-//        setLayout(new FlowLayout());
 
-//        for (int i = 0; i <10; i++) {
-//            add(new JButton(" "+i));
-
-
-//    JButton button = new JButton("Click Me!!");
-//
-//    add(button);
-//
-//    button.addActionListener(new ActionListener() {
-//        @Override
-//        public void actionPerformed(ActionEvent e) {
-//            System.out.println(1);
-//        }
-//    });
-//
-//        add(button, BorderLayout.NORTH);
-//
-//        JTextArea textArea = new JTextArea();
-//        add(textArea,BorderLayout.CENTER);
-
-
-        JTextField textField = new JTextField();
+        textField = new JTextField();
         add(textField, BorderLayout.NORTH);
         textField.setText("Программа загадала число от 1 до 10");
-textField.setEditable(false);
+        textField.setEditable(false);
+        textField.setHorizontalAlignment(SwingConstants.CENTER);
 
-         Font font = new Font("Arial", Font.PLAIN, 24);
+        Font font = new Font("Arial", Font.PLAIN, 20);
         textField.setFont(font);
 
-        JPanel buttonsPanel = new JPanel(new GridLayout(1,4));
+        JPanel buttonsPanel = new JPanel(new GridLayout(1, 10));
         buttonsPanel.setBackground(Color.BLUE);
-        add(buttonsPanel,BorderLayout.CENTER);
+        add(buttonsPanel, BorderLayout.CENTER);
+
+        for (int i = 1; i <= 10; i++) {
+            JButton button = new JButton(String.valueOf(i));
+            button.setFont(font);
+            buttonsPanel.add(button);
+            int buttonIndex = i;
+            button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    tryToAnswer(buttonIndex);
+                }
+            });
+
+
+        }
+        JButton buttonRepeat = new JButton("Повторить");
+        add(buttonRepeat, BorderLayout.SOUTH);
+        buttonRepeat.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
 //
 //        JButton button1 = new JButton("1");
 //        JButton button2 = new JButton("2");
@@ -87,5 +90,17 @@ textField.setEditable(false);
 //        });
 
         setVisible(true);
+    }
+
+    public void tryToAnswer(int answer) {
+        if (answer < randomNubmer) {
+            textField.setText("Не угадали! Загаданное число больше!");
+            return;
+        }
+        if (answer > randomNubmer) {
+            textField.setText("Не угадали! Загаданное число меньше!");
+            return;
+        }
+        textField.setText("Вы угадали!!! Ответ: " + randomNubmer);
     }
 }
---
+++ src/main/java/Lesson8/MapWindow.java
@@ -0,0 +1,75 @@
+package Lesson8;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MapWindow extends JFrame {
+    private int x = 100;
+    private int y = 100;
+    private int size = 40;
+
+    public MapWindow() {
+        setTitle("Карта");
+        setBounds(600, 200, 600, 600);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(null);
+        mainPanel.setBackground(Color.decode("#008800"));
+        add(mainPanel, BorderLayout.CENTER);
+
+        JPanel buttonsPanel = new JPanel(new GridLayout(1, 2));
+        buttonsPanel.setPreferredSize(new Dimension(1, 100));
+        add(buttonsPanel, BorderLayout.SOUTH);
+
+        JButton buttonLeft = new JButton("Left");
+        buttonsPanel.add(buttonLeft);
+        JButton buttonRight = new JButton("Right");
+        buttonsPanel.add(buttonRight);
+
+        JButton buttonUp = new JButton("Up");
+        buttonsPanel.add(buttonUp);
+        JButton buttonDown = new JButton("Down");
+        buttonsPanel.add(buttonDown);
+
+        JPanel rectPanel = new JPanel();
+        rectPanel.setBounds(x, y, size, size);
+        rectPanel.setBackground(Color.WHITE);
+        mainPanel.add(rectPanel);
+
+
+        buttonUp.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                y -= 10;
+                rectPanel.setBounds(x, y, size, size);
+            }
+        });
+        buttonDown.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                y += 10;
+                rectPanel.setBounds(x, y, size, size);
+            }
+        });
+        buttonLeft.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                x -= 10;
+                rectPanel.setBounds(x, y, size, size);
+            }
+        });
+        buttonRight.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                x += 10;
+                rectPanel.setBounds(x, y, size, size);
+            }
+        });
+
+        setVisible(true);
+    }
+}
